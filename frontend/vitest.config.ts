b85ab3dcb07e19d55a5abf1f0c01a5501226d1ea
Sean Carr Geoffrey <54399662+SeanCarr1@users.noreply.github.com>
2025-07-18 08:11:52 +0800

    fix frontend test configs

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import viteConfig from './vite.config'
 
 export default defineConfig({
+  ...viteConfig,
   plugins: [vue()],
   test: {
     environment: 'jsdom',
